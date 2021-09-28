d851f641f932346036dd02bb885369285c165f5f
kongxiangbo <kongxiangbo@xiaomi.com>
2021-09-28 13:50:21 +0800

    fix: ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import App from './App.svelte';
+import T1 from './test_ts/t1.svelte'
 
-const app = new App({
+// const app = new App({
+// 	target: document.body,
+// 	props: {
+// 		name: 'world'
+// 	}
+// });
+
+const app = new T1({
 	target: document.body,
 	props: {
 		name: 'world'
 	}
-});
+})
 
 export default app;
---
+++ src/test_ts/t1.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+	interface User {
+		name: string,
+		age: 18
+	}
+
+	interface Say {
+		(name: string): void
+	}
+
+	type Play = (name: string) => void
+
+	const say:Say = (name) => { console.log(name) }
+
+	const play:Play = (name) => { console.log('I\'m playing, name is ' + name) }
+
+	say('xiaowang')
+	play('xiaozhang')
+</script>
+
+<div>
+	<h1>Welcome!</h1>
+</div>
